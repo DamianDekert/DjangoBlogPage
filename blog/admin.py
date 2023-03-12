6c7896e01dbb6e5b90807279781c55dd56ca33fb
Damian Dekert <damdekert@gmail.com>
2023-03-12 20:23:51 +0100

    Page with data model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Author, Post, Tag
+
 # Register your models here.
+
+class PostSlug(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('author', 'title', 'date')
+    list_filter = ('author', 'tag', 'date', )
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email',)
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostSlug)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+from django.core.validators import MinLengthValidator
+
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+    
+class Tag(models.Model):
+
+    caption = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.caption}'
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True, auto_now_add=False)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts', null=True)
+    tag = models.ManyToManyField(Tag)
+ 
+    def __str__(self) -> str:
+        return f'{self.title}, {self.date}'
+
+
+
